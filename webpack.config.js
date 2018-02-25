18d932dd03bc3f9411aa73d32226d2bc37621ef9
gevgeny <e.gluhotorenko@gmail.com>
2018-02-25 15:11:30 +0300

    add site

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
+var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
   output: {
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'docs')
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -18,6 +20,6 @@ module.exports = {
       },
       template: './index.html'
     }),
-    new CopyWebpackPlugin([{from:'./img',to:'./'} ])
+    new CopyWebpackPlugin([{from:'./img',to:path.resolve(__dirname, 'docs')} ])
   ]
 };
